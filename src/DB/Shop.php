07bdfd134404affef431ce2f40340866b949f45e
Petr Křehlík <pet@lqd.cz>
2023-04-21 15:04:24 +0200

    shop entity

--- src/DB/Shop.php
+++ src/DB/Shop.php
@@ -6,17 +6,16 @@ use StORM\Entity;
 
 /**
  * Represents standalone shop using same database
- * @index{"name":"shop_unique_code","unique":true,"columns":["code"]}
  */
 class Shop extends Entity
 {
 	/**
 	 * @column
 	 */
-	public string $code;
+	public string $name;
 
 	/**
 	 * @column
 	 */
-	public string $name;
+	public string $baseUrl;
 }
--- src/DB/ShopRepository.php
+++ src/DB/ShopRepository.php
@@ -2,11 +2,41 @@
 
 namespace Base\DB;
 
+use Nette\Http\Request;
+use Nette\Utils\Strings;
+use StORM\DIConnection;
 use StORM\Repository;
+use StORM\SchemaManager;
 
 /**
  * @extends \StORM\Repository<\Base\DB\Shop>
  */
 class ShopRepository extends Repository
 {
+	public function __construct(DIConnection $connection, SchemaManager $schemaManager, private readonly Request $request)
+	{
+		parent::__construct($connection, $schemaManager);
+	}
+
+	/**
+	 * Returns shop by code in GET parameter or parse from domain
+	 */
+	public function getSelectedShop(): Shop|null
+	{
+		$code = $this->request->getQuery('shop');
+
+		if ($code) {
+			return $this->many()->where('this.uuid', $code)->first();
+		}
+
+		$host = $this->request->getUrl()->getHost();
+
+		foreach ($this->many() as $shop) {
+			if (Strings::contains(Strings::lower($host), Strings::lower($shop->baseUrl))) {
+				return $shop;
+			}
+		}
+
+		return null;
+	}
 }
