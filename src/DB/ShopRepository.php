1f2ceab18b77dab0275b250944cc815fda2af6ea
Petr Křehlík <pet@lqd.cz>
2023-04-25 15:14:52 +0200

    shop entity

--- src/DB/ShopRepository.php
+++ src/DB/ShopRepository.php
@@ -3,25 +3,14 @@
 namespace Base\DB;
 
 use Base\Repository\IGeneralRepository;
-use Nette\Http\Request;
-use Nette\Utils\Strings;
 use StORM\Collection;
-use StORM\DIConnection;
 use StORM\Repository;
-use StORM\SchemaManager;
 
 /**
  * @extends \StORM\Repository<\Base\DB\Shop>
  */
 class ShopRepository extends Repository implements IGeneralRepository
 {
-	private Shop|null $selectedShop;
-
-	public function __construct(DIConnection $connection, SchemaManager $schemaManager, private readonly Request $request)
-	{
-		parent::__construct($connection, $schemaManager);
-	}
-
 	/**
 	 * @inheritDoc
 	 */
@@ -35,31 +24,5 @@ class ShopRepository extends Repository implements IGeneralRepository
 		unset($includeHidden);
 
 		return $this->many()->orderBy(['this.name']);
-	}
-
-	/**
-	 * Returns shop by code in GET parameter or parse from domain
-	 */
-	public function getSelectedShop(): Shop|null
-	{
-		if (isset($this->selectedShop)) {
-			return $this->selectedShop;
-		}
-
-		$code = $this->request->getQuery('shop');
-
-		if ($code) {
-			return $this->selectedShop ??= $this->many()->where('this.uuid', $code)->first();
-		}
-
-		$host = $this->request->getUrl()->getHost();
-
-		foreach ($this->many() as $shop) {
-			if (Strings::contains(Strings::lower($host), Strings::lower($shop->baseUrl))) {
-				return $this->selectedShop ??= $shop;
-			}
-		}
-
-		return null;
 	}
 }
--- src/ShopsConfig.php
+++ src/ShopsConfig.php
@@ -2,9 +2,20 @@
 
 namespace Base;
 
+use Base\DB\Shop;
+use Base\DB\ShopRepository;
+use Nette\Http\Request;
+use Nette\Utils\Strings;
+
 class ShopsConfig
 {
 	protected \stdClass $config;
+
+	private Shop|null $selectedShop;
+
+	public function __construct(private readonly ShopRepository $shopRepository, private readonly Request $request,)
+	{
+	}
 
 	public function setConfig(\stdClass $config): void
 	{
@@ -14,5 +25,31 @@ class ShopsConfig
 	public function getShopSpecificProperties(): \stdClass
 	{
 		return $this->config->shopSpecificProperties;
+	}
+
+	/**
+	 * Returns shop by code in GET parameter or parse from domain
+	 */
+	public function getSelectedShop(): Shop|null
+	{
+		if (isset($this->selectedShop)) {
+			return $this->selectedShop;
+		}
+
+		$code = $this->request->getQuery('shop');
+
+		if ($code) {
+			return $this->selectedShop ??= $this->shopRepository->many()->where('this.uuid', $code)->first();
+		}
+
+		$host = $this->request->getUrl()->getHost();
+
+		foreach ($this->shopRepository->many() as $shop) {
+			if (Strings::contains(Strings::lower($host), Strings::lower($shop->baseUrl))) {
+				return $this->selectedShop ??= $shop;
+			}
+		}
+
+		return null;
 	}
 }
