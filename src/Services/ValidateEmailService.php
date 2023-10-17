c92c37407f9bcbe2a6b59923fcda36e803a59b17
Petr Krehlik <73128826+petr6@users.noreply.github.com>
2023-10-17 16:04:21 +0200

    feat: new ValidateEmailService

---
+++ src/Services/ValidateEmailService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Base\Services;
+
+use Base\Bridges\AutoWireService;
+use Egulias\EmailValidator\EmailValidator;
+use Egulias\EmailValidator\Validation\DNSCheckValidation;
+use Egulias\EmailValidator\Validation\MultipleValidationWithAnd;
+use Egulias\EmailValidator\Validation\RFCValidation;
+
+class ValidateEmailService implements AutoWireService
+{
+	protected EmailValidator $validator;
+
+	protected MultipleValidationWithAnd $weakConditions;
+
+	protected MultipleValidationWithAnd $strictConditions;
+
+	public function __construct()
+	{
+		$this->validator = new EmailValidator();
+		$this->weakConditions = new MultipleValidationWithAnd([
+			new RFCValidation(),
+		]);
+		$this->strictConditions = new MultipleValidationWithAnd([
+			new RFCValidation(),
+			new DNSCheckValidation(),
+		]);
+	}
+
+	/**
+	 * @deprecated If possible, use "validate"
+	 */
+	public function validateWeakly(string $email): bool
+	{
+		\trigger_deprecation('liquiddesign/package', '2.0', 'Using "%s" is deprecated, use "%s" instead.', 'validateWeakly', 'validate');
+
+		return $this->validator->isValid($email, $this->weakConditions);
+	}
+
+	public function validate(string $email): bool
+	{
+		return $this->validator->isValid($email, $this->strictConditions);
+	}
+}
